c3dc50110f4b95f17c3257cdb4af446032ab9b1c
13Hitendra <hacom134@gmail.com>
2023-10-23 17:37:58 +0530

    Atempts change

--- Number_Guess_Game/Number_Guess_Game.java
+++ Number_Guess_Game/Number_Guess_Game.java
@@ -1,40 +1,84 @@
-import java.util.*;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Number_Guess_Game {
+
+    private static final int LOWER_BOUND = 1;
+    private static final int UPPER_BOUND = 100;
+    private static final int ATTEMPTS_LIMIT = 4;
+
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        String play = "yes";
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        //while loop to determine if we are going to play the game again
-        while(play.equals("yes")){
-            Random rand = new Random();
-            int randNum = rand.nextInt(100);
-            int guess = -1;
-            int tries = 0;
-
-            //while loop to check if the game is over 
-            while(guess != randNum)
-            {
-                System.out.println("Guess a number between 1 and 100: ");
-                guess = reader.nextInt();
-                tries++;
-
-                if(guess == randNum)
-                {
-                    System.out.println("Great! you guessed the number!");
-                    System.out.println("it only took you "+tries+" guesses!");
-                    System.out.println("would you like to play again? Yes or No: ");
-                    play = reader.next().toLowerCase();
-
-                }
-                else if(guess > randNum){
-                    System.out.println("Your guess is too high, try again.");
-                }
-                else{
-                    System.out.println("your guess is too low, try again.");
-                }
+        while (true) {
+            playRound(random, scanner);
+            if (!askToPlayAgain(scanner)) {
+                break;
             }
         }
-        reader.close();
-    }    
+
+        System.out.println("Thanks for playing! Goodbye.");
+        scanner.close();
+    }
+
+    private static void playRound(Random random, Scanner scanner) {
+        int targetNumber = random.nextInt(UPPER_BOUND - LOWER_BOUND + 1) + LOWER_BOUND;
+        int remainingAttempts = ATTEMPTS_LIMIT;
+
+        System.out.println("Guess a number between " + LOWER_BOUND + " and " + UPPER_BOUND + ":");
+        System.out.println("You have "+ATTEMPTS_LIMIT+ " Attempts to guess Correct number");
+
+
+        while (remainingAttempts > 0) {
+            int userGuess = getUserGuess(scanner);
+
+            if (userGuess == targetNumber) {
+                System.out.println("Congratulations! You guessed the number.");
+                return;
+            } else if (userGuess > targetNumber) {
+                System.out.println("Your guess is too high.  " );
+            } else {
+                System.out.println("Your guess is too low.  " );
+            }
+
+            remainingAttempts--;
+        }
+
+        System.out.println("Sorry, you're out of attempts. The correct number was " + targetNumber + ".");
+    }
+
+    private static int getUserGuess(Scanner scanner) {
+        int userGuess = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                userGuess = scanner.nextInt();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                scanner.next(); // Consume the invalid input
+            }
+        }
+
+        return userGuess;
+    }
+
+    private static boolean askToPlayAgain(Scanner scanner) {
+        System.out.println("Would you like to play again? (Yes/No):");
+
+        while (true) {
+            String response = scanner.next().toLowerCase();
+
+            if (response.equals("yes")) {
+                return true;
+            } else if (response.equals("no")) {
+                return false;
+            } else {
+                System.out.println("Invalid input. Please enter 'Yes' or 'No'.");
+            }
+        }
+    }
 }
